a189e01c68d55da6f2a83a8b56e415a656884b25
WidleyMayk <widleywmss@gmail.com>
2025-07-02 19:23:19 -0300

    Atualização do projeto: ajustes e melhorias

--- backend/src/tarefas/models/taskModel.js
+++ backend/src/tarefas/models/taskModel.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const moment = require('moment-timezone');
 
-//Fuso horário 
+// Fuso horário 
 const timezone = 'America/Recife';
+
 const taskSchema = new mongoose.Schema({
   titulo: {
     type: String,
@@ -11,38 +12,65 @@ const taskSchema = new mongoose.Schema({
   },
   descricao: {
     type: String,
-    trim: true
+    trim: true,
+    maxlength: [255, 'A descrição não pode ter mais de 255 caracteres']
   },
   status: {
     type: String,
     enum: ['pendente', 'em_andamento', 'concluida'],
+    message: 'Status inválido',
     default: 'pendente'
   },
   prioridade: {
     type: String,
     enum: ['baixa', 'media', 'alta'],
-    default: 'media'
+    default: 'media',
+    required: [true, 'A prioridade é obrigatória']
   },
-  dataVencimento: {
-    type: Date
+  data: {
+    type: Date,
+    required: [true, 'A data é obrigatória'],
+
+    // Validação para garantir que a data de vencimento seja maior que a data atual
+    validate: function (value) {
+      // Verifica se a data selecionada é maior que a data atual
+      const today = moment().tz(timezone).startOf('day');
+      // Converte a data selecionada para o fuso horário do Brasil
+      const dataSelecionada = moment(value).tz(timezone).startOf('day');
+      // Verifica se a data selecionada é maior que a data atual
+      return dataSelecionada.isAfter(today, 'day');
+    }
   },
-  responsavel: {
+  responsaveis: {
     type: String,
-    required: true
+    required: [true, 'O responsável é obrigatório'],
+
+    // Validação para garantir que o responsável seja um usuário válido
+    validate: {
+      // Validação para garantir que o responsável seja um usuário válido
+      validator: arrayLimit,
+    }
   },
-  dataCriacao: {
+  criadoEm: {
     type: Date,
     default: () => moment().tz(timezone).toDate()
   },
-  dataAtualizacao: {
+  atualizadoEm: {
     type: Date,
     default: () => moment().tz(timezone).toDate()
   }
 });
 
-// Middleware para atualizar dataAtualizacao automaticamente
+function arrayLimit(val) {
+  return val.length > 0;
+}
+
 taskSchema.pre('save', function(next) {
-  this.dataAtualizacao = moment().tz(timezone).toDate();
+  this.atualizadoEm = moment().tz(timezone).toDate();
   next();
 });
+
+const Tarefa = mongoose.model('Tarefa', taskSchema);
+
+module.exports = Tarefa;
 
